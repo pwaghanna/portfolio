b8c34b3f815cf1cbd0791f65869ae939835b4e5f
Pranav Waghanna <pwaghanna@yahoo.com>
2025-12-18 16:19:05 -0500

    deploy setup

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  
+
+  
+  // Option 2: For project repo (e.g., portfolio)
+  basePath: '/portfolio',
+  assetPrefix: '/portfolio',
+  
+  images: {
+    unoptimized: true
+  },
+  
+  // Ensure trailing slashes for better compatibility
+  trailingSlash: true,
+}
+
+module.exports = nextConfig
